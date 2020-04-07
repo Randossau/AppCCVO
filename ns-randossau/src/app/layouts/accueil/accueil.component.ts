39f6002adecf1390973b29dc917fb72dafc5b126
F43d3rik <f.holtermann@icloud.com>
2020-04-07 09:18:16 +0200

    Creation premier composant

---
+++ ns-randossau/src/app/layouts/accueil/accueil.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ns-accueil',
+  templateUrl: './accueil.component.html',
+  styleUrls: ['./accueil.component.css'],
+  moduleId: module.id,
+})
+
+
+export class AccueilComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
