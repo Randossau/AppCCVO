5fd32949d3e7dabc64db371b64f7b4dda8867ba2
AlaaAD86 <fangstein@gmail.com>
2020-04-14 13:01:56 +0200

    creation bottom bar

--- ns-randossau/src/app/app-routing.module.ts
+++ ns-randossau/src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from "@angular/core";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { Routes } from "@angular/router";
 import { AccueilComponent } from "./layouts/accueil/accueil.component";
+import { ParcoursComponent } from "./layouts/parcours/parcours.component";
+import { IncidentsComponent } from "./layouts/incidents/incidents.component";
+import { GpsComponent } from "./layouts/gps/gps.component";
 
 
 const routes: Routes = [
     // { path: "", redirectTo: "/items", pathMatch: "full" }
-    { path: "", component:AccueilComponent }
+    { path: "", component:AccueilComponent },
+    { path: "parcours", component:ParcoursComponent },
+    { path: "incidents", component:IncidentsComponent },
+    { path: "gps", component:GpsComponent }
 
 ];
 
